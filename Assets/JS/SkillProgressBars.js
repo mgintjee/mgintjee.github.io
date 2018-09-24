5b5fb3ae51a350a1dee1fb14e5ae984784ae6a43
Matthew Gintjee <mgintjee@gmail.com>
2018-09-23 20:12:06 -0700

    Merged onScroll listeners

--- Assets/JS/SkillProgressBars.js
+++ Assets/JS/SkillProgressBars.js
@@ -1,27 +1,27 @@
 var FrontEndSkills = [
-		["LanguageProgressCSS",        6.0],
-		["LanguageProgressHTML",       5.0],
-		["LanguageProgressJavaScript", 6.5],
-		["LanguageProgressJQuery",     2.5]
-	],
-	BackEndSkills = [
-		["LanguageProgressJava",      8.0],
-		["LanguageProgressPython",    8.5],
-		["LanguageProgressMySQL",     6.5],
-		["LanguageProgressCsharp",    6.5],
-		["LanguageProgressCplusplus", 5.0],
-		["LanguageProgressC",         4.5]
-	]
-	FrontEndLength = FrontEndSkills.length,
-	BackEndLength = BackEndSkills.length;
+["LanguageProgressCSS",        6.0],
+["LanguageProgressHTML",       5.0],
+["LanguageProgressJavaScript", 6.5],
+["LanguageProgressJQuery",     2.5]
+],
+BackEndSkills = [
+["LanguageProgressJava",      8.0],
+["LanguageProgressPython",    8.5],
+["LanguageProgressMySQL",     6.5],
+["LanguageProgressCsharp",    6.5],
+["LanguageProgressCplusplus", 5.0],
+["LanguageProgressC",         4.5]
+]
+FrontEndLength = FrontEndSkills.length,
+BackEndLength = BackEndSkills.length;
 
 var FrontEndOverall = 0,
-	BackEndOverall = 0;
+BackEndOverall = 0;
 
 for(i=0; i<FrontEndLength;++i){
 	var ID = FrontEndSkills[i][0];
 	var Value = FrontEndSkills[i][1];
-	var Percentage = (25 * Value) / 10.0;
+	var Percentage = (35 * Value) / 10.0;
 	var ProgressBar = document.getElementById(ID);
 	var SkillID = ID+"P";
 	var SkillP = document.getElementById(SkillID);
@@ -33,26 +33,25 @@ for(i=0; i<FrontEndLength;++i){
 var SkillID = "OverallProgressFrontEndP";
 var SkillP = document.getElementById(SkillID);
 var ProgressBar = document.getElementById("OverallProgressFrontEnd");
-var Percentage = (35 * FrontEndOverall) / (10 * FrontEndLength);
+var Percentage = (45 * FrontEndOverall) / (10 * FrontEndLength);
 ProgressBar.setAttribute("style", "width:" + Percentage + "vw;");
-SkillP.innerHTML = FrontEndOverall + "/" + (10 * FrontEndLength);
+SkillP.innerHTML = FrontEndOverall + " / " + (10 * FrontEndLength);
 
 for(i=0; i<BackEndLength;++i){
 	var ID = BackEndSkills[i][0];
 	var Value = BackEndSkills[i][1];
-	var Percentage = (25 * Value) / 10.0;
+	var Percentage = (35 * Value) / 10.0;
 	var ProgressBar = document.getElementById(ID);
 	var SkillID = ID+"P";
 	var SkillP = document.getElementById(SkillID);
 	SkillP.innerHTML = Value;
 	ProgressBar.setAttribute("style", "width:" + Percentage + "vw;");
 	BackEndOverall += Value;
-	console.log(ID,":",Value)
 }
 
 var SkillID = "OverallProgressBackEndP";
 var SkillP = document.getElementById(SkillID);
 var ProgressBar = document.getElementById("OverallProgressBackEnd");
-var Percentage = (35 * BackEndOverall) / (10 * BackEndLength);
+var Percentage = (45 * BackEndOverall) / (10 * BackEndLength);
 ProgressBar.setAttribute("style", "width:" + Percentage + "vw;");
-SkillP.innerHTML = BackEndOverall+ "/" + (10 * BackEndLength);
+SkillP.innerHTML = BackEndOverall+ " / " + (10 * BackEndLength);
---
+++ Assets/JS/WindowScrollingListener.js
@@ -0,0 +1,113 @@
+// Some Globals for this script
+var Headers = ["HeaderAbout", "HeaderEducation", "HeaderProjects", "HeaderSkills", "HeaderContact"],
+NavLinks = ["NavAbout", "NavEducation", "NavProjects", "NavSkills", "NavContact"],
+HeadersLength = Headers.length,
+BelowTopOfScreen = true,
+CurrentHeader  = -1,
+PreviousHeader = -1;
+
+// Initial Call
+HandleHeaderInView();
+
+// On Scroll Listener
+window.onscroll = function(){
+  HandleHeaderCompression();
+  console.log (BelowTopOfScreen);
+  if(BelowTopOfScreen){
+    HandleHeaderInView();
+  }
+};
+
+//  Checks each of the headers to see if it is in view. 
+// Remembers the last previous one
+function HandleHeaderInView(){
+  for(i=0; i<HeadersLength; i++) {
+    var Header = document.getElementById(Headers[i]);
+    if (isElementInViewport(Header)){
+      console.log(PreviousHeader, CurrentHeader, i);
+      if(PreviousHeader != i){
+        CurrentHeader = i;
+      }
+    }
+  }
+  if(PreviousHeader != CurrentHeader){
+    UpdateNav(CurrentHeader);
+    PreviousHeader = CurrentHeader;
+  }
+}
+
+function UpdateNav(index){
+  for(i=0; i < HeadersLength; ++i ){
+    var NavLink = document.getElementById(NavLinks[i]);
+// Not Visible
+if(i != index ){
+  NavLink.setAttribute("style","background-image: linear-gradient(to bottom, var(--RedLight ), var(--RedDark )); background-repeat:  no-repeat; background-size: 100% 100%;");
+}
+//Visible
+else{
+  NavLink.setAttribute("style","background-image: linear-gradient(to bottom, var(--RedLight ), var(--GrayDark )); background-repeat:  no-repeat; background-size: 100% 100%;");
+}
+}
+}
+
+function isElementInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document. documentElement.clientWidth)
+    );
+}
+
+function HandleHeaderCompression() {
+  BelowTopOfScreen = document.body.scrollTop > 1 || document.documentElement.scrollTop > 1;
+
+  /* User Is NOT At Top Of Page */
+  if (BelowTopOfScreen) {
+    CompressHeader();
+  } 
+  /* User Is At Top Of Page */
+  else {
+    ExpandHeader();
+  }
+}
+
+function CompressHeader(){
+  var HeaderNav = document.getElementById("WrapperHeader");
+  var HeaderContent = document.getElementById("HeaderContainer");
+  var AboutContent = document.getElementById("ContentAbout");
+  var Spacer = document.getElementById("Spacer");
+
+  HeaderNav.style.height = "10vh";
+  Spacer.style.height= "10vh";
+  Spacer.style.margin = "0";
+  HeaderContainer.style.display = "none";
+}
+
+function ExpandHeader(){
+  var HeaderNav = document.getElementById("WrapperHeader");
+  var HeaderContent = document.getElementById("HeaderContainer");
+  var AboutContent = document.getElementById("ContentAbout");
+  var Spacer = document.getElementById("Spacer");
+
+  HeaderNav.style.height = "90vh";
+  Spacer.style.height= "10vh";
+  Spacer.style.marginTop = "90vh";
+  HeaderContainer.style.display = "flex";
+
+  ClearNavLinkSelection();
+}
+
+function ClearNavLinkSelection(){
+  for(i=0; i < HeadersLength; ++i ){
+    var NavLink = document.getElementById(NavLinks[i]);
+    NavLink.setAttribute("style","background-image: linear-gradient(to bottom, var(--RedLight ), var(--RedDark )); background-repeat:  no-repeat; background-size: 100% 100%;");
+    if(NavLink.classList.contains("selected")){
+      NavLink.classList.remove("selected");
+
+    }
+  }
+  CurrentHeader = -1;
+  PreviousHeader = -1;
+}
