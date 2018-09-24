dc914c3c96a6fc8b0abf68723e2ff8367befe4d7
Matthew Gintjee <mgintjee@gmail.com>
2018-09-23 20:28:15 -0700

    adjusted skill bars

--- Assets/JS/SkillProgressBars.js
+++ Assets/JS/SkillProgressBars.js
@@ -21,7 +21,7 @@ BackEndOverall = 0;
 for(i=0; i<FrontEndLength;++i){
 	var ID = FrontEndSkills[i][0];
 	var Value = FrontEndSkills[i][1];
-	var Percentage = (35 * Value) / 10.0;
+	var Percentage = (30 * Value) / 10.0;
 	var ProgressBar = document.getElementById(ID);
 	var SkillID = ID+"P";
 	var SkillP = document.getElementById(SkillID);
@@ -33,14 +33,14 @@ for(i=0; i<FrontEndLength;++i){
 var SkillID = "OverallProgressFrontEndP";
 var SkillP = document.getElementById(SkillID);
 var ProgressBar = document.getElementById("OverallProgressFrontEnd");
-var Percentage = (45 * FrontEndOverall) / (10 * FrontEndLength);
+var Percentage = (40 * FrontEndOverall) / (10 * FrontEndLength);
 ProgressBar.setAttribute("style", "width:" + Percentage + "vw;");
 SkillP.innerHTML = FrontEndOverall + " / " + (10 * FrontEndLength);
 
 for(i=0; i<BackEndLength;++i){
 	var ID = BackEndSkills[i][0];
 	var Value = BackEndSkills[i][1];
-	var Percentage = (35 * Value) / 10.0;
+	var Percentage = (30 * Value) / 10.0;
 	var ProgressBar = document.getElementById(ID);
 	var SkillID = ID+"P";
 	var SkillP = document.getElementById(SkillID);
@@ -52,6 +52,6 @@ for(i=0; i<BackEndLength;++i){
 var SkillID = "OverallProgressBackEndP";
 var SkillP = document.getElementById(SkillID);
 var ProgressBar = document.getElementById("OverallProgressBackEnd");
-var Percentage = (45 * BackEndOverall) / (10 * BackEndLength);
+var Percentage = (40 * BackEndOverall) / (10 * BackEndLength);
 ProgressBar.setAttribute("style", "width:" + Percentage + "vw;");
 SkillP.innerHTML = BackEndOverall+ " / " + (10 * BackEndLength);
